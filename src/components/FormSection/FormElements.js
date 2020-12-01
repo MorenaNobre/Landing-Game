3a0548bcb6ec6bc1bd376db52ae695a693726f25
MorenaNobre <morenagnobre@gmail.com>
2020-12-01 12:55:36 -0300

    FormSection-Responsive

--- src/components/FormSection/FormElements.js
+++ src/components/FormSection/FormElements.js
@@ -24,6 +24,25 @@ export const Form = styled.div`
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    transition: 0.3s ease-in;
+    border-radius: 0;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    transition: 0.3s ease-in;
+    border-radius: 0;
+  }
+
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+    transition: 0.3s ease-in;
+    border-radius: 0;
+  }
 `;
 
 export const DescriptionContainer = styled.div`
@@ -31,6 +50,17 @@ export const DescriptionContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    transition: 0.3s ease-in;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 100%;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const Title = styled.h1`
@@ -40,6 +70,14 @@ export const Title = styled.h1`
   font-size: 35px;
   color: var(--aqua-color);
   text-transform: uppercase;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 250px;
+    font-size: 20px;
+    text-align: center;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const Description = styled.p`
@@ -49,6 +87,25 @@ export const Description = styled.p`
   font-size: 20px;
   text-align: left;
   margin-top: 42px;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 90%;
+    margin: 0;
+    transition: 0.3s ease-in;
+    text-align: center;
+    font-size: 16px;
+    transition: 0.3s ease-in;
+  }
+
+  @media screen and (max-width: 960px) {
+    width: 90%;
+    margin: 0;
+    transition: 0.3s ease-in;
+    text-align: center;
+    font-size: 16px;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const InputWrapper = styled.div`
@@ -56,6 +113,18 @@ export const InputWrapper = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 556px;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    transition: 0.3s ease-in;
+    height: 120px;
+    justify-content: space-around;
+  }
 `;
 
 export const Input = styled.input`
@@ -68,6 +137,12 @@ export const Input = styled.input`
   font-family: "Open Sans", sans-serif;
   font-size: 18px;
   border-radius: 8px;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 90%;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const TextAreaContainer = styled.textarea`
@@ -79,25 +154,47 @@ export const TextAreaContainer = styled.textarea`
   font-size: 18px;
   margin-top: -128px;
   border-radius: 8px;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    width: 90%;;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const ButtonWrapper = styled.div`
   width: 556px;
   margin-top: -98px;
+  transition: 0.3s ease-in;
+
+  @media screen and (max-width: 768px) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    transition: 0.3s ease-in;
+  }
 `;
 
 export const Button = styled.button`
   width: 249px;
   height: 48px;
-  background: #63C7A9;
+  background: #63c7a9;
   text-align: center;
   color: var(--white-color);
   font-size: 18px;
-  font-family: 'Open Sans', sans-serif;
+  font-family: "Open Sans", sans-serif;
   text-transform: uppercase;
   border: none;
   border-radius: 8px;
-  &:hover{
+  transition: 0.3s ease-in;
+
+  &:hover {
     background: #48937d;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 90%;
+    transition: 0.3s ease-in;
   }
 `;
