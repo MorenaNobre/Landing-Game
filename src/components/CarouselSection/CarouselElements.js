30ac520dcc229a7686104db4c0d750bffa14000e
MorenaNobre <morenagnobre@gmail.com>
2020-12-01 20:45:35 -0300

    CarouselWeb-done/started CarouselResponsive

--- src/components/CarouselSection/CarouselElements.js
+++ src/components/CarouselSection/CarouselElements.js
@@ -1,7 +1,69 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import ImgGrant from "../../images/Grant.png";
 
 export const CarouselContainer = styled.div`
+  margin: auto;
+  padding: 192px 40px 40px 40px;
   width: 100%;
   height: 1147px;
   background: var(--dark-bg);
-`
+`;
+
+export const CardWrapper = styled.div`
+  margin: 20px;
+`;
+
+export const CharacterCard = styled.div`
+  background: #5f9ea0;
+  margin: 10px;
+  padding: 2%;
+  position: relative;
+  height: 700px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const MainCard = styled.div`
+  width: 387px;
+  height: 580px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border-radius: 27px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+`;
+
+export const CharacterWrapper = styled.div`
+  width: 324px;
+  height: 438px;
+  background: var(--dark-bg);
+  border: 2px solid var(--white-color);
+  border-radius: 100px;
+  position: absolute;
+  top: 0;
+  margin-top: -64px;
+`;
+
+export const Grant = styled.div`
+  background: url(${ImgGrant}) no-repeat;
+  width: 358px;
+  height: 520px;
+  background-size: 368px;
+  background-position-y: center;
+  background-position-x: center;
+  position: absolute;
+  margin-top: -324px;
+`;
+
+export const Description = styled.div`
+  width: 326px;
+  height: 153px;
+  font-size: 20px;
+  text-align: left;
+  font-family: 'Open Sans', sans-serif;
+  color: var(--dark-bg);
+  margin-bottom: -372px;
+`;
--- src/components/CarouselSection/index.js
+++ src/components/CarouselSection/index.js
@@ -1,12 +1,85 @@
-import React from 'react'
-import { CarouselContainer } from './CarouselElements'
+import React, { Component } from "react";
+import Slider from "react-slick";
+import {
+  CarouselContainer,
+  CardWrapper,
+  CharacterCard,
+  MainCard,
+  CharacterWrapper,
+  Grant,
+  Description,
+} from "./CarouselElements";
 
-const CarouselSection = () => {
-  return (
-    <CarouselContainer>
-      
-    </CarouselContainer>
-  )
+export default class Responsive extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    };
+    return (
+      <CarouselContainer>
+        <style>{cssstyle}</style>
+        <Slider {...settings}>
+          <CardWrapper>
+            <CharacterCard>
+              <MainCard>
+                <CharacterWrapper></CharacterWrapper>
+                <Grant />
+                <Description>
+                  A Camerata foi apenas os dois no início, e suas fileiras nunca
+                  foram destinadas a exceder um número a ser contado em uma mão.
+                </Description>
+              </MainCard>
+            </CharacterCard>
+          </CardWrapper>
+          <CardWrapper>
+            <CharacterCard></CharacterCard>
+          </CardWrapper>
+          <CardWrapper>
+            <CharacterCard></CharacterCard>
+          </CardWrapper>
+          <CardWrapper>
+            <CharacterCard></CharacterCard>
+          </CardWrapper>
+        </Slider>
+      </CarouselContainer>
+    );
+  }
 }
 
-export default CarouselSection
+const cssstyle = `
+.slick-next:before, .slick-prev:before {
+    color: #fff;
+    font-size: 30px;
+}
+`;
